d6bd70f3ca8d9dbd411d747b853fa5ce278dd067
dhwani444 <dhwani.vaishnav@torontomu.ca>
2024-02-25 22:44:46 -0500

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ function promptForName() {
 
 function enlargeImage() {
   var imageElement = document.getElementById("myImage");
-  imageElement.style.width = "300px"; 
+  imageElement.style.width = "100%"; 
 }
 
 function createNavigationMenu() {
