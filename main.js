0f8455b23816fc3252e250185658a535b05426d6
dhwani444 <dhwani.vaishnav@torontomu.ca>
2024-02-25 23:12:32 -0500

    Adding comments

--- main.js
+++ main.js
@@ -1,22 +1,30 @@
+
+// function for asking user for name
 function promptForName() {
+  
   var userName = prompt("Please enter your name:");
   var outputElement = document.getElementById("output");
+  // acknowledgement with personalized name
   outputElement.innerHTML = "Hello, " + userName + "!";
+
 }
 
+// function for changing the size of image
 function toggleImageSize() {
+  // setting an ID name to reference elsewhere
   var imageElement = document.getElementById("myImage");
   var enlargedWidth = 300; 
-
   imageElement.style.width = enlargedWidth + "px";
 }
 
-
+// function for creating the navigation menu
 function createNavigationMenu() {
   var navItems = document.getElementsByClassName("nav-item");
 
+  // for loop for  mouseover and mouseout as per requirements
   for (var i = 0; i < navItems.length; i++) {
       navItems[i].addEventListener("mouseover", function () {
+        // hover colour set to purple
           this.style.backgroundColor = "purple";
       });
 
@@ -26,8 +34,9 @@ function createNavigationMenu() {
   }
 }
 
+// adding event listeners to execute asking for name and image size functions
 document.getElementById("nameButton").addEventListener("click", promptForName);
 document.getElementById("myImage").addEventListener("click", toggleImageSize);
 
-
+// calling the function
 createNavigationMenu();
